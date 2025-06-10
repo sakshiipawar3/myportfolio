487e4c26556c6f9fdf684798c2ff67d90e365455
sakshiipawar3 <pawarsakshi792@gmail.com>
2025-06-10 08:36:32 +0530

    Added .gitignore and project files

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 
 import Navbar from './components/Navbar';
 import Home from './components/Home';
@@ -7,18 +8,23 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
-
 function App() {
   return (
-    <div className="scroll-smooth">
+    <motion.div
+      className="scroll-smooth"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
       <Navbar />
       <Home />
       <About />
       <Skills />
       <Projects />
-       <Contact />
-        <Footer />
-    </div>
+      <Contact />
+      <Footer />
+    </motion.div>
   );
 }
 
